a8d3c4ffbba99c537920d90d96751a6d0039373c
privatewasant <wasan@digio.co.th>
2023-11-12 00:39:02 +0700

    add duck database module.

--- app/src/main/java/com/psycho/duck/MainActivity.kt
+++ app/src/main/java/com/psycho/duck/MainActivity.kt
@@ -2,9 +2,13 @@ package com.psycho.duck
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.psycho.database.managers.di.DatabaseManagerSDK
+import com.psycho.database.managers.di.DatabaseManagerService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DatabaseManagerSDK.init(this)
+        DatabaseManagerService.provider.databaseManager.netManager.get()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Duck"
 include(":app")
+include(":database")
